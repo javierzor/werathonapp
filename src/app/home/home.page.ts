533f6e3fe5aae044eca6d3cd4f0ea13af4ac7063
javierzor <javier20450@gmail.com>
2022-03-19 02:13:36 -0500

    agg: pais, restricciones de filtro al registro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  secretKey = "123456&Descryption";
+  verificarloginemail: any;
+  menuderechosuperior:boolean=false;
 
-  constructor() {}
+  constructor() 
+  {
+    this.funcionverificarlogin();
+  }
+
+  funcionverificarlogin(){
+    this.verificarloginemail=localStorage.getItem('email');
+    this.verificarloginemail= this.decrypt(this.verificarloginemail);
+    console.log('this.verificarlogin', this.verificarloginemail);
+    if(this.verificarloginemail!=null||this.verificarloginemail!='false')
+     {
+      console.log('Bienvenido:',this.verificarloginemail);
+    }
+  }
+
+    async ngOnInit() {
+     this.funcionverificarlogin();
+  }
+
+  ONCHANGEmenuderechosuperior(){
+    if(this.menuderechosuperior==false){
+      this.menuderechosuperior=true;
+    }
+    else{
+      this.menuderechosuperior=false;
+    }
+  }
+
+  ONCHANGEclickenelcontent(){
+    this.menuderechosuperior=false;
+
+  }
+
+  decrypt(textToDecrypt : string){
+    return CryptoJS.AES.decrypt(textToDecrypt, this.secretKey.trim()).toString(CryptoJS.enc.Utf8);
+  }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -20,13 +20,16 @@ export class LoginPage implements OnInit {
   registercorreo: any;
   registropassword: any;
   isLoggedIn: boolean;
-  usuario:any;
+  usuarioderegistro:any;
   registropasswordrepetida:any;
   countrySelected:any;
   countryData: any;
   nombre: any;
   apellido: any;
   aceptaterminos: boolean = false;
+  paisdataseleccionado: any;
+  colocouncorreoperonoelpunto:any;
+  registrandoseconarrobaenelusuario: boolean = false;
   constructor(
     private router: Router,
     private variosservicios: VariosService,
@@ -48,7 +51,7 @@ export class LoginPage implements OnInit {
     // this.variosservicios.loading18segundos("Verificando Información...");
     // this.variosservicios.quitarloading();
     
-     this.countryData=this.paises.countryData    
+     this.countryData=this.paises.countryData;    
 
   }
 
@@ -69,48 +72,125 @@ export class LoginPage implements OnInit {
     this.step="registro";
   }
 
+  CHANGEusuarioderegistro(){
+      if(this.usuarioderegistro.includes('@')==true){
+          this.registrandoseconarrobaenelusuario=true;
+      }
+      else{
+        this.registrandoseconarrobaenelusuario=false;
+
+      }
+  }
+
+  CHANGEusuarioocorreo(){
+    // console.log('contiene arrobas, true y false: ',this.loginuser.includes('@'));
+    if(this.loginuser.includes('@')==true){
+      this.colocouncorreoperonoelpunto=true;
+          if(this.loginuser.includes('.')==true){
+            this.colocouncorreoperonoelpunto=false;
+          }
+    }
+    else{
+      this.colocouncorreoperonoelpunto=false;
+    }
+  }
 
  async logear(){
 
+   console.log('contiene arrobas, true y false: ',this.loginuser.includes('@'));
+   if(this.loginuser.includes('@')==true){
+      console.log('contiene arrobas la cadena de texto para logear');
+      this.colocouncorreoperonoelpunto=true;
+          if(this.loginuser.includes('.')==true){
+            this.colocouncorreoperonoelpunto=false;
+            console.log('Aparte de arrobas tambien tiene su PUNTO (.)');
+            this.variosservicios.loading2segundos("El inicio de sección por correo electronico esta desabilitada");
+            this.variosservicios.presentToast("..::Porfavor ingrese su nombre de usuario::..");
+          }
+          else{
+            this.colocouncorreoperonoelpunto=false;
+            this.variosservicios.presentToast("..::Ingrese un correo valido.::..");
+          }
+
+   }
+
+   else {
+      this.colocouncorreoperonoelpunto=false;
+      console.log('Cadena de texto SIN ARROBAS!.');
       var dataoptimaconsultaruser = {
         nombre_solicitud:'werathonappinicio',
         username: this.loginuser,
         password: this.loginpassword
       }
-    this.variosservicios.loading18segundos("Verificando...");
+      this.variosservicios.loading18segundos("Verificando...");
+      console.log('el usuario intenta logear con esta data,',dataoptimaconsultaruser);
+            this.variosservicios.variasfunciones(dataoptimaconsultaruser).subscribe(async( res: any ) =>{
+              console.log(' respuesta optimaconsultaruser ',res);
+              if(res!='credencialesincorrectas'&&res!=null){
+                this.variosservicios.loading2segundos("Verificacíon exitosa...");
+                this.variosservicios.quitarloading();
+                localStorage.setItem('isLoggedIn', 'true');
+                localStorage.setItem('tipo_cuenta', this.encrypt(res.tipo_cuenta));
+                localStorage.setItem('email', this.encrypt(res.email));
+                localStorage.setItem('username', this.encrypt(res.username));
+                this.router.navigate(['home']);
+                // this.menu.enable(true);
+              }
+              else{
+                this.variosservicios.quitarloading();
+                this.variosservicios.loading2segundos("Verifique sus credenciales.");
+              }
 
-    console.log('el usuario intenta logear con esta data,',dataoptimaconsultaruser);
+        });
 
-          this.variosservicios.variasfunciones(dataoptimaconsultaruser).subscribe(async( res: any ) =>{
-            console.log(' respuesta optimaconsultaruser ',res);
-            if(res!='credencialesincorrectas'&&res!=null){
-              // this.json.isloggedin='si';
-              // this.json.username=res.username;
-              // this.json.tipo_cuenta=res.tipo_cuenta;
-              this.variosservicios.loading2segundos("Verificacíon exitosa...");
-              this.variosservicios.quitarloading();
-              // this.menu.enable(true);
-              // this.router.navigateByUrl('/app/tabs/schedule');
-            }
-            else{
-              this.variosservicios.quitarloading();
-              this.variosservicios.loading2segundos("Verifique sus credenciales.");
-            }
-            
+   }
 
-      });
+
 
     }
 
 
-
+    ONCHANGEPAIS(event){
+      console.log('index', event.target.value);
+      this.paisdataseleccionado= this.countryData[event.target.value];
+      console.log('paisData',this.paisdataseleccionado);
+    }
     
 
-    registrarsecorreo(){
-    var data = {
+  async  registrarsecorreo(){
+      var datawerathoncreateuser = {
+        create_date: new Date(),
+        nombre_solicitud:'werathoncreateuser',
+        username:this.usuarioderegistro,
+        password:this.registropassword,
+        tipo_cuenta:'0',
+        email:this.registercorreo,
+        name:this.nombre,
+        lastname:this.apellido,
+        paisnombre:this.paisdataseleccionado.name,
+        paisID:this.paisdataseleccionado.id
+      }
+    console.log('el usuario intenta registrarse con esta data,',datawerathoncreateuser);
+    this.variosservicios.loading18segundos("Espere, Se esta registrando su cuenta de usuario...");
+    this.variosservicios.variasfunciones(datawerathoncreateuser).subscribe(async (res: any ) =>{
+      console.log(' respuesta werathoncreateuser ',res);
+      if(res.id>0){
+        this.variosservicios.quitarloading();
+        this.variosservicios.loading2segundos("Registro exitoso, Regirigiendo...");
+        this.variosservicios.presentToast("..::Usuario Registrado correctamente::..");
+        this.router.navigate(['home']);
+      }
+     },
+     error=>{
+      console.log('Errores',error);
+      if(error){
+        this.variosservicios.quitarloading();
+        this.variosservicios.loading2segundos("El usuario o su correo ya fue registrado intente con otro.");
+        this.variosservicios.presentToast("..::intente con otro Usuario/Correo::..");
 
-    }
-    console.log('el usuario intenta registrarse con esta data,',data);
+      }
+     }
+     );
     }
   
 
@@ -128,5 +208,8 @@ export class LoginPage implements OnInit {
 
   }
 
+  cambioterminos(){
+    console.log('terminos esta:,',this.aceptaterminos);
+  }
 
 }
