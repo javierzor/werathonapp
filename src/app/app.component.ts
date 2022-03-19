3ef1e6d96a1915ecc8e6c78e240cb0472ac297a0
javierzor <javier20450@gmail.com>
2022-03-19 11:29:33 -0500

    Menu lateral con coneccion de ruta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  secretKey = "123456&Descryption";
+  verificarloginemail: any;
+  tipo_cuenta: any;
+  constructor(
+    private menu: MenuController
+  ) 
+  
+  {
+
+  }
+
+  funcionverificarlogin(){
+    this.verificarloginemail=localStorage.getItem('email');
+    this.verificarloginemail= this.decrypt(this.verificarloginemail);
+    console.log('this.verificarlogin', this.verificarloginemail);
+    if(this.verificarloginemail!=null||this.verificarloginemail!='false')
+     {
+      console.log('Bienvenido:',this.verificarloginemail);
+    }
+  }
+
+  funcionverificartipocuenta(){
+    this.tipo_cuenta=localStorage.getItem('email');
+    this.tipo_cuenta= this.decrypt(this.tipo_cuenta);
+    console.log('this.verificarlogin', this.tipo_cuenta);
+    if(this.tipo_cuenta)
+     {
+      console.log('Bienvenido:',this.tipo_cuenta);
+    }
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  decrypt(textToDecrypt : string){
+    return CryptoJS.AES.decrypt(textToDecrypt, this.secretKey.trim()).toString(CryptoJS.enc.Utf8);
+  }
+
+
 }
